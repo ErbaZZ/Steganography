9f44edb1132544cbc3e681436395ae5b145b6f71
ErbaZZ <weerawat.paw@gmail.com>
2018-05-10 23:55:54 +0700

    Dynamic multiple bits encoding

--- steganography.py
+++ steganography.py
@@ -3,9 +3,6 @@ import sys
 
 from PIL import Image
 
-replace_bits = 1  # must be less than 8 and not too high
-
-
 # Functions from https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
 def text_to_bits(plaintext, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int(binascii.hexlify(plaintext.encode(encoding, errors)), 16))[2:]
@@ -33,47 +30,67 @@ def encode(plaintext, image, outimage):
     # print(text_bin)
     text_size = len(text_bin)
     count = 0
+    endencode = 0
     image.x = image.size[0]
     image.y = image.size[1]
     pixels = image.load()
-    for x in range(0, image.x):
-        for y in range(0, image.y):
-            i = 0
-            colors = [0] * 3
-            for channel in pixels[x, y]:
-                pix_bin = bin(channel)
-                if count < text_size:
-                    # Replace the last bits of the target pixel with the data bit
-                    data_bits = ''
-                    for j in range(0, replace_bits):
-                        data_bits += text_bin[count] if count < text_size else '0'
+    for sub_position in range(1, 8):
+        if endencode >= 8:
+            break
+        for x in range(0, image.x):
+            if endencode >= 8:
+                break
+            for y in range(0, image.y):
+                if endencode >= 8:
+                    break
+                i = 0
+                colors = [0] * 3
+                for channel in pixels[x, y]:
+                    pix_bin = bin(channel)
+                    if count < text_size:
+                        # Replace the bit of the target pixel with the data bit
+                        colors[i] = int(pix_bin[:-sub_position] + text_bin[count] + pix_bin[len(pix_bin)+1-sub_position:], 2)
                         count += 1
-                    colors[i] = int(pix_bin[:-replace_bits] + data_bits, 2)
-                else:
-                    # Replace the last bits of the target pixel with 0
-                    colors[i] = int(pix_bin[:-replace_bits] + '0' * replace_bits, 2)
-                i += 1
-            image.putpixel((x, y), tuple(colors))
+                    else:
+                        # Replace the bit of the target pixel with 0
+                        colors[i] = int(pix_bin[:-sub_position] + '0' + pix_bin[len(pix_bin)+1-sub_position:], 2)
+                        endencode += 1
+                    
+                    i += 1
+                image.putpixel((x, y), tuple(colors))
     image.save(outimage)
     print('Image encoded!')
     return
 
 
 def decode(image):
-    text_ = ''
+    text_bin = ''
     decoded_text = ''
     image.x = image.size[0]
     image.y = image.size[1]
     pixels = image.load()
-    for x in range(0, image.x):
-        for y in range(0, image.y):
-            # Append data from the image with the last bits of each color channel
-            for channel in pixels[x, y]:
-                text_ += bin(channel)[-replace_bits:]
+    enddecode = 0
+    for pos in range(0, 8):
+        if enddecode >= 15:
+            break
+        for x in range(0, image.x):
+            if enddecode >= 15:
+                break
+            for y in range(0, image.y):
+                if enddecode >= 15:
+                    break
+                # Append data from the image with the bits of each color channel
+                for channel in pixels[x, y]:
+                    bit = bin(channel)[len(bin(channel))-1-pos:len(bin(channel))-pos]
+                    text_bin += bit
+                    if str(bit) == '0':
+                        enddecode += 1
+                    else:
+                        enddecode = 0
 
-    for x in range(0, len(text_), 8):
+    for x in range(0, len(text_bin), 8):
         # Keep appending data bits until zeroes are found
-        char_bin = text_[x:x + 8]
+        char_bin = text_bin[x:x + 8]
         if char_bin != '00000000':
             decoded_text += char_bin
         else:
@@ -93,10 +110,10 @@ def print_usage():
 
 
 #   TODO: Accept CLI arguments
-#       1) python steganography.py -e -p <plaintext> -i <input-image> -o <output-image> [-b 1]
-#       2) python steganography.py -e -f <plaintext-file> -i <input-image> -o <output-image> [-b 1]
-#       3) python steganography.py -d -i <input-encoded-image> [-b 1]                                   # Print output text to console
-#       4) python steganography.py -d -i <input-encoded-image> -o <output-text-file> [-b 1]             # Save output to file
+#       1) python steganography.py -e -p <plaintext> -i <input-image> -o <output-image>
+#       2) python steganography.py -e -f <plaintext-file> -i <input-image> -o <output-image>
+#       3) python steganography.py -d -i <input-encoded-image>                                      # Print output text to console
+#       4) python steganography.py -d -i <input-encoded-image> -o <output-text-file>                # Save output to file
 #   Extra:
 #       1) Check image file extension to be only .png and .bmp
 #       2) Show warning and instruction when invalid argument pattern is detected
@@ -127,9 +144,6 @@ elif sys.argv[1] == '-e':
         # Check output
         if sys.argv[i] == '-o':
             output = sys.argv[i + 1]
-        # Check replace bits
-        if sys.argv[i] == '-b':
-            replace_bits = int(sys.argv[i + 1])
 
     img = Image.open(input_image, 'r')
     encode(plaintext, img, output)
@@ -148,9 +162,6 @@ elif sys.argv[1] == '-d':
             outflag = True
             output = sys.argv[i + 1]
             f = open(output, "w+")
-        # Check replace bits
-        if sys.argv[i] == '-b':
-            replace_bits = int(sys.argv[i + 1])
 
     img = Image.open(input_image, 'r')
     outtext = decode(img)
